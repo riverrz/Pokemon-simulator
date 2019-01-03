d2d06dbe788cab769fbf3fc9a741c4e12f318252
shivam123425 <shivam123425@gmail.com>
2019-01-03 15:16:02 +0530

    Create client dir to hold react app, set server boilerplate

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require("express");
+const http = require("http");
+const app = express();
+
+const server = http.createServer(app);
+const PORT = process.env.PORT || 5000;
+
+server.listen(PORT, () => {
+  console.log("Server has started");
+});
