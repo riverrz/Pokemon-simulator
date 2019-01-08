dc67f26a1192274975a5be2cc38687e5920c4867
shivam123425 <shivam123425@gmail.com>
2019-01-08 14:56:20 +0530

    Add attack func to control damagecalc, add helper funcs

--- utils/damagecalc.js
+++ utils/damagecalc.js
@@ -1,64 +1,65 @@
 const moves = require("../data/pokedex/moves.json");
 const pokemon = require("../data/pokedex/pokemon.json");
 
-var Attack_Stat = function(attacker) {
-  if (pokemon[attacker].base.Attack > pokemon[attacker].base.Sp_Attack) {
-    return pokemon[attacker].base.Attack;
+var Attack_Stat = function(attackerIndex) {
+  if (
+    pokemon[attackerIndex].base.Attack > pokemon[attackerIndex].base.Sp_Attack
+  ) {
+    return pokemon[attackerIndex].base.Attack;
   } else {
-    return pokemon[attacker].base.Sp_Attack;
+    return pokemon[attackerIndex].base.Sp_Attack;
   }
 };
 
-var Defense_Stat = function(target) {
-  if (pokemon[target].base.Defense > pokemon[target].base.Sp_Defense) {
-    return pokemon[target].base.Defense;
+var Defense_Stat = function(targetIndex) {
+  if (
+    pokemon[targetIndex].base.Defense > pokemon[targetIndex].base.Sp_Defense
+  ) {
+    return pokemon[targetIndex].base.Defense;
   } else {
-    return pokemon[target].base.Sp_Defense;
+    return pokemon[targetIndex].base.Sp_Defense;
   }
 };
 
-//attacker denotes attacking pokemon
-//target denotes target pokemon
-var damage_calc = function(target, attacker, attack) {
-  let level = pokemon[attacker].level;
-  let Move_Power = moves[attack].power;
-  let AtkStat = Attack_Stat(attacker);
-  let DefStat = Defense_Stat(target);
+//attackerIndex denotes index of attacking pokemon
+//targetIndex denotes index of target pokemon
+var damage_calc = function(targetIndex, attackerIndex, moveIndex) {
+  let level = pokemon[attackerIndex].level;
+  let Move_Power = moves[moveIndex].power;
+  let AtkStat = Attack_Stat(attackerIndex);
+  let DefStat = Defense_Stat(targetIndex);
   let Modifier = 1;
   if (random() < 1) {
     return 0;
   } else {
-    return (
+    return Math.round(
       ((((2 * level) / 5 + 2) * Move_Power * AtkStat) / DefStat / 50 + 2) *
-        Modifier +
-      "%"
+        Modifier
     );
   }
 };
 
-var effect = function() {
+var effect = function(moveIndex) {
   if (
-    moves[attack].effect.chance ||
-    moves[attack].effect.stat ||
-    moves[attack].effect.stages
+    moves[moveIndex].effect.chance ||
+    moves[moveIndex].effect.stat ||
+    moves[moveIndex].effect.stages
   ) {
     return (
       " Chance: " +
-      moves[attack].effect.chance +
+      moves[moveIndex].effect.chance +
       " Stat:" +
-      moves[attack].effect.stat +
+      moves[moveIndex].effect.stat +
       " Stages:" +
-      moves[attack].effect.stages
+      moves[moveIndex].effect.stages
     );
   } else {
-    return moves[attack].effect;
+    return moves[moveIndex].effect;
   }
 };
 
 var random = function() {
   return Math.floor(Math.random() * Math.floor(3));
 };
-
-console.log(damage_calc(1, 1, 1)); // find index of attacker, find index of attack, find index of target
 
 module.exports = damage_calc;
