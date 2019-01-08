eed611a368dacb4e7682e58ae8fa8fcd26799580
shivam123425 <shivam123425@gmail.com>
2019-01-08 13:58:56 +0530

    Use path module to build paths

--- utils/pokemon.js
+++ utils/pokemon.js
@@ -1,12 +1,11 @@
-'use strict';
-
-const fs = require('fs');
-
-let pokedata = fs.readFileSync('../data/pokedex/pokemon.json');
+const fs = require("fs");
+const path = require("path");
+const pathTo = path.join(__dirname, "../data/pokedex/pokemon.json");
+let pokedata = fs.readFileSync(pathTo);
 
 let pokemon = JSON.parse(pokedata);
-for( var i=0;i<pokemon.length;i++){
-console.log("Pokemon Name:"+pokemon[i].name);
-console.log("Pokemon Types:"+pokemon[i].types);
-console.log("Pokemon Health:"+pokemon[i].base.HP);
+for (var i = 0; i < pokemon.length; i++) {
+  console.log("Pokemon Name:" + pokemon[i].name);
+  console.log("Pokemon Type:" + pokemon[i].type);
+  console.log("Pokemon Health:" + pokemon[i].base.HP);
 }
